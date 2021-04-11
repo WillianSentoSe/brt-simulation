bad767946abea221f2457a65707e45bbc4ee77f6
= <viniciusassis2012@gmail.com>
2021-04-11 09:53:10 -0300

    - Adicionando variaveis como parametros

--- exec.sh
+++ exec.sh
@@ -2,5 +2,9 @@
 ittrs=500       # Quantidade de Iterações
 rd_size=100     # Tamanho da Estrada
 max_cars=0      # Quantidade Maxima de Carros
+max_speed=0     # Velocidade Maxima dos Carros
+min_spwn=2      # Numero minima de iterações para gerar novo Carro
+max_spwn=4      # Numero máximo de iterações para gerar novo Carro
+brk_prb=0.25    # Probabilidade de redução de velocidade
 
-./exec $ittrs $rd_size $max_cars
+./exec $ittrs $rd_size $max_cars $max_speed $min_spwn $max_spwn $brk_prb
--- src/configuration/configuration.cpp
+++ src/configuration/configuration.cpp
@@ -4,4 +4,8 @@ Configuration::Configuration(int argc, char **argv) {
     if (argc >= 2) totalIterations = std::stoi(argv[1]);
     if (argc >= 3) roadLength = std::stoi(argv[2]);
     if (argc >= 4) carCount = std::stoi(argv[3]);
+    if (argc >= 5) maxSpeed = std::stoi(argv[4]);
+    if (argc >= 6) newCarIntervalMin = std::stoi(argv[5]);
+    if (argc >= 7) newCarIntervalMax = std::stoi(argv[6]);
+    if (argc >= 8) breakProbability = std::stoi(argv[7]);
 }
